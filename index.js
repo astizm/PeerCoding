0460fcca40faa65bf35cf251158aebe3c5586d61
astizm <asti-maera@live.nmit.ac.nz>
2020-03-03 09:38:30 +1300

    lernd var

---
+++ index.js
@@ -0,0 +1,18 @@
+var numberOne = 50
+var numberTwo = 25
+console.log(numberOne + numberTwo)
+
+var sum = numberOne + numberTwo
+console.log( "sum = " + sum);
+
+var sub = numberOne-numberTwo
+console.log( "sub = " + sub); 
+
+var div = numberOne/numberTwo
+console.log( "div =" + div);
+
+var rem = numberOne%numberTwo
+console.log( "rem =" + rem);
+
+var mul = numberOne * numberTwo
+console.log( "mul =" + mul);
