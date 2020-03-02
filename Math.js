4b219195589e75aa5a03a292f7f801a5fe5327d2
astizm <asti-maera@live.nmit.ac.nz>
2020-03-03 10:15:26 +1300

    new mafs

--- Math.js
+++ Math.js
@@ -1,6 +1,11 @@
 var numberOne = 1
-var numberTwo = 1
-var numberThree = 1
-var numberFour = 1.3
+var numberTwo = 2
+var numberThree = 3
+var numberFour = 4
 var x = x
 console.log (x = numberOne/numberTwo+numberThree/numberOne-numberFour%numberFour)
+
+console.log (numberOne+numberTwo*numberThree);
+console.log (numberOne+(numberTwo*numberThree));
+console.log ((numberOne+numberTwo)*numberThree);
+console.log (numberOne*numberThree+numberTwo*numberThree);
